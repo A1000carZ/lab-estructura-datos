ddff39d31caac556daa95a487a33d6dc7f85d1e2
Amilcar <a1000carzh@gmail.com>
2023-05-31 22:32:36 -0600

    added some changes..

--- trees/main.cpp
+++ trees/main.cpp
@@ -3,76 +3,159 @@
 
 using namespace std;
 #define COUNT 10
- 
+
 // A binary tree node
-class Node {
+class Node
+{
 public:
-    int data;
+    int value;
     Node *left, *right;
- 
-    /* Constructor that allocates a new node with the
-    given data and NULL left and right pointers. */
-    Node(int data)
+    Node(int value)
     {
-        this->data = data;
+        this->value = value;
         this->left = NULL;
         this->right = NULL;
     }
 };
- 
-// Function to print binary tree in 2D
-// It does reverse inorder traversal
-void print2DUtil(Node* root, int space)
+
+void print2DUtil(Node *root, int space)
 {
-    // Base case
     if (root == NULL)
         return;
- 
-    // Increase distance between levels
+
     space += COUNT;
- 
-    // Process right child first
+
     print2DUtil(root->right, space);
- 
-    // Print current node after space
-    // count
+
     cout << endl;
     for (int i = COUNT; i < space; i++)
         cout << " ";
-    cout << root->data << "\n";
- 
-    // Process left child
+    cout << root->value<< "\n";
+
     print2DUtil(root->left, space);
 }
- 
+
 // Wrapper over print2DUtil()
-void print2D(Node* root)
+void print2D(Node *root)
 {
     // Pass initial space count as 0
     print2DUtil(root, 0);
 }
- 
-// Driver code
+void crearABB(Node *, int dato);
+void preOrden(Node *);
+void inOrden(Node *);
+void postOrden(Node *);
+
+using namespace std;
+
 int main()
 {
-    Node* root = new Node(1);
-    root->left = new Node(2);
-    root->right = new Node(3);
- 
-    root->left->left = new Node(4);
-    root->left->right = new Node(5);
-    root->right->left = new Node(6);
-    root->right->right = new Node(7);
- 
-    root->left->left->left = new Node(8);
-    root->left->left->right = new Node(9);
-    root->left->right->left = new Node(10);
-    root->left->right->right = new Node(11);
-    root->right->left->left = new Node(12);
-    root->right->left->right = new Node(13);
-    root->right->right->right = new Node(15);
- 
-    print2D(root);
- 
-    return 0;
+    int opc, n;
+    Node *raiz = new Node(0);
+
+    while (1)
+    {
+        print2D(raiz);
+        cout << "1. Insertar\n";
+        cout << "2. Pre-Orden\n";
+        cout << "3. In-Orden\n";
+        cout << "4. Post-Orden\n";
+        cout << "Selecciona una opciÛn: ";
+        cin >> opc;
+        cout << "\n";
+
+        switch (opc)
+        {
+        case 1:
+            if (raiz == NULL)
+            {
+                cout << "Ingresa el valor de la raiz: ";
+                cin >> raiz->value;
+            }
+            else
+            {
+                cout << "Ingresa el valor del nodo: ";
+                cin >> n;
+                crearABB(raiz, n);
+            }
+            break;
+
+        case 2:
+            cout << "Recorrido Pre-Orden:\n";
+            preOrden(raiz);
+            cout << endl;
+            break;
+
+        case 3:
+            cout << "Recorrido In-Orden:\n";
+            inOrden(raiz);
+            cout << endl;
+            break;
+
+        case 4:
+            cout << "Recorrido Post-Orden:\n";
+            postOrden(raiz);
+            cout << endl;
+            break;
+        }
+    }
+}
+
+void crearABB(Node *Nodo, int dato)
+{
+    Node *nuevo;
+    if (dato < Nodo->value)
+    {
+        if (Nodo->left == NULL)
+        {
+            nuevo = new Node(0);
+            Nodo->left = nuevo;
+            nuevo->value = dato;
+        }
+        else
+            crearABB(Nodo->left, dato);
+    }
+    else if (dato > Nodo->value)
+    {
+        if (Nodo->right == NULL)
+        {
+            nuevo = new Node(0);
+            Nodo->right = nuevo;
+            nuevo->value = dato;
+        }
+        else
+            crearABB(Nodo->right, dato);
+    }
+    else
+        cout << "El dato ya existe en el ·rbol\n";
+}
+
+void preOrden(Node *raiz)
+{
+    if (raiz != NULL)
+    {
+        cout << raiz->value << " ";
+        preOrden(raiz->left);
+        preOrden(raiz->right);
+    }
+}
+
+void inOrden(Node *raiz)
+{
+    if (raiz != NULL)
+    {
+        inOrden(raiz->left);
+        cout << raiz->value << " ";
+        inOrden(raiz->right);
+    }
+}
+
+void postOrden(Node *raiz)
+{
+    if (raiz != NULL)
+    {
+        postOrden(raiz->left);
+        postOrden(raiz->right);
+        cout << raiz->value << " ";
+    }
 }
